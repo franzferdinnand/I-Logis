efce65628b611281e52fea7e7c55056d710accfd
franzferdinnand <serhiyturchyn@gmail.com>
2023-08-30 11:53:55 +0200

    more added

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,14 @@
-from django.views.generic import TemplateView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, DetailView
 
 
 class IndexView(TemplateView):
-    template_name = "index.html"
+    template_name = "index_mutated.html"
     http_method_names = ["get"]
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    template_name = "profile/user_profile.html"
+    model = get_user_model()
+    queryset = get_user_model().objects.all()
