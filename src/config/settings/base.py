cdb29231f9cb952fd80ff933c7bfa256be7af612
franzferdinnand <serhiyturchyn@gmail.com>
2023-09-03 18:32:21 +0200

    django downgraded, registration via google added

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from datetime import timedelta
-
+import os
 import django
-from decouple import config
+from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 from django.utils.encoding import force_str
 django.utils.encoding.force_text = force_str
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-
+load_dotenv(find_dotenv())
 # Application definition
 
 
@@ -77,11 +76,6 @@ AUTHENTICATION_BACKENDS = (
 
 
 SOCIAL_AUTH_URL_NAMESPACE = "social"
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config("GOOGLE_KEY")
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config("GOOGLE_SECRET")
-
-
 
 
 TEMPLATES = [
@@ -170,9 +164,11 @@ EMAIL_FAIL_SILENTLY = False
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
-# EMAIL_HOST = config("EMAIL_HOST")
-# EMAIL_HOST_USER = config("EMAIL_USER")
-# EMAIL_HOST_PASSWORD = config("EMAIL_PASSWORD")
-# DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 EMAIL_PORT = 587
 
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv("GOOGLE_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv("GOOGLE_SECRET")
